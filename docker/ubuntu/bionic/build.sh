af8c3e731d3dbd831a55f68ce6a8af6f614d3c20
Blair Davidosn <blair.joel.davidosn@gmail.com>
2019-02-12 22:11:35 +0800

    working Docker file

--- docker/ubuntu/bionic/build.sh
+++ docker/ubuntu/bionic/build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build --build-arg uid="$1" --build-arg username="$2" --build-arg passwd="$3" -t "clion:jetbrains-bionic-1.0.0" .
+docker build  --no-cache --build-arg uid="$1" --build-arg username="$2" --build-arg passwd="$3" -t "clion:jetbrains-bionic-1.0.0" .
 
